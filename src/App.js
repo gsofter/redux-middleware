25c3ac8c2ed1d42bd42639728e0b51c4a773782f
guru <guru.softwaremaster@mail.ru>
2020-04-21 07:15:59 +0300

    Add: redux-logger applied

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './modules'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import loggerMiddleware from './lib/loggerMiddleware'
+// import loggerMiddleware from './lib/loggerMiddleware'
+import { createLogger } from 'redux-logger'
+const logger = createLogger()
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(loggerMiddleware)),
+  composeWithDevTools(applyMiddleware(logger)),
 )
 
 function App() {
